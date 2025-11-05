6561dfc10d11b808b936e0489c4cfff3e5a7464f
helagrz <hela.grz8@gmail.com>
2025-11-05 05:25:19 -0800

    Testing
    Testing hunger and level updates

--- critter_class.h
+++ critter_class.h
@@ -4,7 +4,7 @@ using namespace std;
 class critter {
 public:
     string name, personality;
-    int mood=3, hunger=5, level=11;
+    int mood=3, hunger=2, level=10;
     critter() {
         cout << "A new critter has been born!" << endl;
         name="";
@@ -18,14 +18,38 @@ public:
     void status() {
         cout << "   STATUS"<< endl;
         cout<<"---------------"<<endl;
-        cout << "Hunger "<<status_str(hunger)<<endl;
-        cout << "LV "<< to_string((level-(level%10))/10) << " "<<status_str(level)<<endl;
+        cout << update_hunger(0)<<endl;
+        cout << update_level(0)<<endl;
         cout<<"---------------"<<endl<<endl;
     }
 
+    string update_level(int amount) {
+        string x = "";
+        level += amount;
+        x= x+ "LV " + to_string((level-(level%10))/10) +" "+status_str(level);
+        return x;
+    }
+    string update_hunger(int amount) {
+        string x = "";
+        hunger += amount;
+        if (hunger>10) {
+            hunger = 10;
+            x = name + " is full! \n";
+            x += "Hunger ";
+            for (int i=0; i<10;i++) {
+                x += "■";
+            }
+        }
+        else {
+            x= x+ "Hunger " + status_str(hunger);
+        }
+        return x;
+    }
+
+private:
     string status_str(int n) {
         string x = "";
-        if (n>10) {
+        if (n>=10) {
             n = n%10;
         }
         for (int i=0; i<n;i++) {
--- main.cpp
+++ main.cpp
@@ -8,5 +8,16 @@ int main() {
     a.name = "Bebe";
     a.info();
     a.status();
+    cout << "Testing feeding and hunger:"<<endl;
+    cout << "+ 2"<<endl<<a.update_hunger(2)<<endl;
+    cout << "+ 5"<<endl<<a.update_hunger(5)<<endl;
+    cout << "+ 4"<<endl<<a.update_hunger(4)<<endl;
+    cout << "+ 4"<<endl<<a.update_hunger(4)<<endl;
+
+    cout << "Testing levels:"<<endl;
+    cout << "+ 4"<<endl<<a.update_level(4)<<endl;
+    cout << "+ 12"<<endl<<a.update_level(12)<<endl;
+    cout << "+ 4"<<endl<<a.update_level(4)<<endl;
+    cout << "+ 42"<<endl<<a.update_level(42)<<endl;
     return 0;
 }
